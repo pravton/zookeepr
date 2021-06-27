1d5b2fc9dbb16bfa85d0cf712cca053465a8d7dd
PravTon <clintonsf@outlook.com>
2021-06-27 16:26:01 -0400

    zookeeper funtionalities and modularized the code

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,4 +1,5 @@
 const $displayArea = document.querySelector('#display-area');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 const printResults = resultArr => {
   console.log(resultArr);
@@ -20,8 +21,28 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = () => {
-  fetch('/api/zookeepers')
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObj = { name, age };
+
+  getZookeepers(zookeeperObj);
+};
+
+const getZookeepers = (formData = {}) => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
+  fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
         return alert('Error: ' + response.statusText);
@@ -34,4 +55,6 @@ const getZookeepers = () => {
     });
 };
 
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
+
